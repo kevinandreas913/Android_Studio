322442c4906b8566a0f93c8dbcf4142209f03b1a
kevinandreas913 <kevinandreas913@gmail.com>
2025-05-22 17:44:04 +0700

    hotel app

---
+++ HotelApp/app/src/main/java/com/ak222102468/hotelapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ak222102468.hotelapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView _imageView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        _imageView1 = findViewById(R.id.imageView1);
+
+        String imageUrl = "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
+    }
+}
