967d430f5b93b38f33798f94b37fa186573cbdff
kevinandreas913 <kevinandreas913@gmail.com>
2025-05-27 22:09:07 +0700

    recyclerViewA

---
+++ RecyclerViewA/app/src/main/java/com/ak222102468/recyclerviewa/MainActivity.java
@@ -0,0 +1,72 @@
+package com.ak222102468.recyclerviewa;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView _recyclerView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        _recyclerView1 = (RecyclerView)findViewById(R.id.recyclerView1);
+
+        List<NegaraModel> negaraModelList = new ArrayList<>();
+
+        NegaraModel nm = new NegaraModel();
+        nm.setNama("Albania");
+        nm.setDeskripsi("Negara ini menggunakan bendera berwarna merah.");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Albania-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Hungary");
+        nm.setDeskripsi("Negara ini bernama Hungaria dalam bahasa Indonesia.");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Hungary-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Slovenia");
+        nm.setDeskripsi("Belum ada deskripsi untuk negara ini. Silahkan ditambahkan bagi yang memiliki informasi.");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Slovenia-Flag-icon.png\n");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Belgia");
+        nm.setDeskripsi("Namanya Belgia dalam bahasa Indonesia. Negara ini terdapat di Eropa.");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Belgium-Flag-icon.png\n");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Canada");
+        nm.setDeskripsi("Negara ini terdapat di sebelah utara engara Amerika Serikat.");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Canada-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+        _recyclerView1.setLayoutManager(lm);
+
+        NegaraAdapter na = new NegaraAdapter(getApplicationContext(), negaraModelList);
+        _recyclerView1.setAdapter(na);
+
+    }
+}
