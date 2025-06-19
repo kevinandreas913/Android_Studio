7510f7facb488ffd40b01702ec1d8b2894a9263c
kevinandreas913 <kevinandreas913@gmail.com>
2025-06-19 17:04:27 +0700

    cuaca

---
+++ Cuaca/app/src/main/java/com/ak222102468/cuaca/RootModel.java
@@ -0,0 +1,28 @@
+package com.ak222102468.cuaca;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RootModel {
+    @SerializedName("list")
+    private List<ListModel> listModelList;
+    @SerializedName("city")
+    private CityModel cityModel;
+
+    public RootModel(){ }
+
+    public List<ListModel> getListModelList() { return listModelList; }
+
+    public void setListModelList(List<ListModel> listModelList){
+        this.listModelList = listModelList;
+    }
+
+    public CityModel getCityModel() {
+        return cityModel;
+    }
+
+    public void setCityModel(CityModel cityModel) {
+        this.cityModel = cityModel;
+    }
+}
